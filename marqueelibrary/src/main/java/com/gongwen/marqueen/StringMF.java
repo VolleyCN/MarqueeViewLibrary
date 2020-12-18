3bdb86bba81a45e4f3f0ef0c9146dfcbd9bffe4d
mengzhang <volleycn@sina.com>
2020-12-18 15:50:41 +0800

    显示优化

--- marqueelibrary/src/main/java/com/gongwen/marqueen/StringMF.java
+++ marqueelibrary/src/main/java/com/gongwen/marqueen/StringMF.java
@@ -16,6 +16,9 @@ import java.util.List;
  * Created by GongWen on 17/9/15.
  */
 public class StringMF extends SimpleMF<String> {
+    protected int mw;
+    protected float textSize = 15;
+
     public StringMF(Context context) {
         super(context);
     }
@@ -23,19 +26,27 @@ public class StringMF extends SimpleMF<String> {
     private String content;
 
     public void setData(String content) {
+        setData(content, mw, textSize);
+    }
+
+    public void setData(String content, int mw, float textSize) {
         this.content = content;
-        if (mMarqueeView != null && mMarqueeView.getWidth() > 0) {
-            if (mMarqueeView instanceof SimpleMarqueeView) {
-                SimpleMarqueeView simpleMarqueeView = (SimpleMarqueeView) mMarqueeView;
-                fitContent(content, simpleMarqueeView);
-            }
+        if (mw > 0 && textSize > 0) {
+            fitContent(content, mw, textSize);
         }
     }
 
     @Override
     protected void attachedToMarqueeView(final MarqueeView marqueeView) {
         super.attachedToMarqueeView(marqueeView);
-        if (TextUtils.isEmpty(content)) return;
+        try {
+            addOnGlobalLayoutListener(marqueeView);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addOnGlobalLayoutListener(final MarqueeView marqueeView) {
         try {
             marqueeView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                 @Override
@@ -46,10 +57,12 @@ public class StringMF extends SimpleMF<String> {
                         } else {
                             marqueeView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                         }
+                        mw = marqueeView.getWidth();
                         if (marqueeView instanceof SimpleMarqueeView) {
                             SimpleMarqueeView simpleMarqueeView = (SimpleMarqueeView) marqueeView;
-                            fitContent(content, simpleMarqueeView);
+                            textSize = simpleMarqueeView.getTextSize();
                         }
+                        fitContent(content, mw, textSize);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -60,10 +73,11 @@ public class StringMF extends SimpleMF<String> {
         }
     }
 
-    private void fitContent(String message, SimpleMarqueeView marqueeView) {
+    protected void fitContent(String message, int mW, float textSize) {
         try {
-            float textSize = marqueeView.getTextSize();
-            int mW = marqueeView.getWidth();
+            this.mw = mW;
+            this.textSize = textSize;
+            if (TextUtils.isEmpty(message)) return;
             if (message.contains("\n")) {
                 String replace = message.replace("\n", "@#&");
                 String[] splits = replace.split("@#&");
@@ -80,7 +94,7 @@ public class StringMF extends SimpleMF<String> {
         }
     }
 
-    private List<String> splitMessage(String message, int mW, float textSize) {
+    protected List<String> splitMessage(String message, int mW, float textSize) {
         List<String> contents = new ArrayList<>();
         try {
             int messageLength = message.length();
@@ -102,15 +116,20 @@ public class StringMF extends SimpleMF<String> {
         return contents;
     }
 
-    public int getScreenWidth() {
-        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-        if (wm == null) return -1;
-        Point point = new Point();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            wm.getDefaultDisplay().getRealSize(point);
-        } else {
-            wm.getDefaultDisplay().getSize(point);
+    protected int getScreenWidth() {
+        try {
+            WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+            if (wm == null) return -1;
+            Point point = new Point();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                wm.getDefaultDisplay().getRealSize(point);
+            } else {
+                wm.getDefaultDisplay().getSize(point);
+            }
+            return point.x;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return point.x;
+        return 100;
     }
 }
